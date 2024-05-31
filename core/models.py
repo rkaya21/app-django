a832b55a78d8517a65a740dfbe95be090449b5ae
recepeneskaya <rkaya21@posta.pau.edu.tr>
2024-05-31 13:58:29 +0300

    feat: create general settings model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from .views import contact_form, contact
+
 
 # Create your models here.
+
+
+class GeneralSettings(models.Model):
+    name = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+    )
+    description = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+    )
+    parameter = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+    )
+    update_date = models.DateTimeField(
+        blank=True,
+        auto_now=True,
+    )
+    create_date = models.DateTimeField(
+        blank=True,
+        auto_now_add=True,
+    )
